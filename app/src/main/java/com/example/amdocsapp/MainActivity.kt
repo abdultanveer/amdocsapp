99cabfb2355d2f59e8e5e9dcaa745cbbc7081469
abdul <abdul.tanveer@gmail.com>
2024-07-15 12:15:39 +0530

    onfocus, onclick listeners

--- app/src/main/java/com/example/amdocsapp/MainActivity.kt
+++ app/src/main/java/com/example/amdocsapp/MainActivity.kt
@@ -3,49 +3,43 @@ package com.example.amdocsapp
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnFocusChangeListener {
+    lateinit var cancelBtn:Button
+    lateinit var emailEt:EditText
+
     //var TAG = "MainActivity"
     var TAG = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)   //inflation
         Log.i(TAG,"oncreate")
+
+        emailEt = findViewById(R.id.etEmail)
+    //dive into the activity_main xml file which you inffflated recently and find a view in
     }
 
     override fun onStart() {
         super.onStart()
         Log.v(TAG,"onstart")
+        emailEt.setOnFocusChangeListener(this);
+
+        cancelBtn = findViewById(R.id.btnCancel)
+        cancelBtn.setOnClickListener(View.OnClickListener {
+            Log.i(TAG,"cancel btn clicked")
+
+        })
     }
 
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG,"onpause")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.w(TAG,"onresume")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.e(TAG,"onstop")
-
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(TAG,"ondestroy")
-
-    }
 
     fun clickHandler(view: View) {
         //View myView = new View()
@@ -53,6 +47,16 @@ class MainActivity : AppCompatActivity() {
         Snackbar.make(myView,"archived email",Snackbar.LENGTH_LONG)
             .setAction("undo", View.OnClickListener {  })
             .show()
+    }
+
+    override fun onFocusChange(emailEdittext: View?, hasFocus: Boolean) {
+        if (hasFocus){
+            Log.i(TAG,"has focus")
+        }
+        else {
+            Log.i(TAG,"lost focus")
+
+        }
     }
 
 
