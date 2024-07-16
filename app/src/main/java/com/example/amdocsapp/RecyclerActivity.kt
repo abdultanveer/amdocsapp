1e2a377eaaaf111a87c4ae021fb038d03a860071
abdul <abdul.tanveer@gmail.com>
2024-07-16 11:45:46 +0530

    supporting diff screenn sizes n orientations

--- app/src/main/java/com/example/amdocsapp/RecyclerActivity.kt
+++ app/src/main/java/com/example/amdocsapp/RecyclerActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.amdocsapp.databinding.ActivityRecyclerBinding
 
 class RecyclerActivity : AppCompatActivity() {
-    lateinit var recyclerView: RecyclerView
+
+    lateinit var  binding: ActivityRecyclerBinding
 
     var countries = arrayOf("India","usa","uk","australia","India","usa","uk","australia","India","usa","uk","australia"
     ,"India","usa","uk","australia","India","usa","uk","australia","India","usa","uk","australia",
@@ -16,11 +18,13 @@ class RecyclerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycler)
-        recyclerView = findViewById(R.id.recyclerView)
+        binding = ActivityRecyclerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        
 
        var myadapter = CountriesAdapter(countries)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = myadapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = myadapter
     }
 }
