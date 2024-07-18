9a396b8202f48fa37efb325514495533361276c7
abdul <abdul.tanveer@gmail.com>
2024-07-18 10:58:27 +0530

    roomdb-1

---
+++ app/src/main/java/com/example/amdocsapp/Item.kt
@@ -0,0 +1,21 @@
+package com.example.amdocsapp
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "name")
+
+    val itemName: String,
+    @ColumnInfo(name = "price")
+
+    val itemPrice: Double,
+    @ColumnInfo(name = "quantity")
+
+    val quantityInStock: Int
+)
